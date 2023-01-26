2ce15232cfcb049c007b07d70ef05e257c0542b6
Jaehun Kim <60066262+dev4jaehunkim@users.noreply.github.com>
2023-01-26 10:55:59 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -1,26 +1,19 @@
 function solution(chicken) {
-    let result = 0;
-    let temp = chicken % 10; // 108개를 주문하면 쿠폰은 1개가 남는다.
-    let answer = parseInt(chicken / 10); // 108개의 닭 주문, 108 쿠폰 발급
-    result += answer;
-    answer += temp;
+    // 서비스 치킨의 수를 누적할당해 줄 변수
+    let answer = 0;
+    
+    // 치킨을 주문한 수만큼 쿠폰 발행
+    let coupon = chicken
 
-    while(1) {
-        // 초기값에서 남는 쿠폰을 다시 구한다.
-        temp = answer % 10;
+    // 쿠폰이 10개 이상인 경우 서비스 치킨 주문을 반복
+    while(coupon >= 10){
+        // 서비스 치킨 주문하고 리턴할 변수에 누적
+        const service = parseInt(coupon/10);
+        answer = answer + service;
         
-        // 서비스 치킨 주문
-        answer = Math.floor(answer / 10); // 10
-        result += answer;
-        
-        // 쿠폰이 딱 맞아 떨어진다면 남은 쿠폰이 없으므로 반복 중단
-        if (answer === 0) {
-            break;
-        }
-        
-        // 아니라면 새로 발행된 쿠폰과 남은 쿠폰을 합침
-        answer += temp;
+        // 새로 발행된 쿠폰과 이전 회차에서 남았던 쿠폰을 합쳐줌
+        coupon = service + coupon%10
     }
 
-    return (result);
+    return answer;
 }
